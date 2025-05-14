a851e3333c6aff2b5f6c0bcf062116e931cfc8b5
RoviraR <roviraruben@gmail.com>
2025-05-14 20:05:17 +0200

    Update AccountController.cs

--- BlogMultisuarioApp/Controllers/AccountController.cs
+++ BlogMultisuarioApp/Controllers/AccountController.cs
@@ -19,7 +19,7 @@ namespace BlogMultisuarioApp.Controllers
 
             // Redirigir a Cognito para el logout, pasando el parámetro client_id
             var clientId = "6nej8qanjv13q0kobf3glnr9d2"; // Tu Client ID de Cognito
-            var redirectUri = "https://blogmultiusuarioapp-env.eba-h8cibmhk.us-east-1.elasticbeanstalk.com/";  // URL de redirección después de logout
+            var redirectUri = "https://tajama03-env.eba-3se5iiix.us-east-1.elasticbeanstalk.com/";  // URL de redirección después de logout
 
             // La URL de logout de Cognito debe incluir el client_id y la URL de redirección
             var logoutUrl = $"https://us-east-1vtFErItEI.auth.us-east-1.amazoncognito.com/logout?client_id={clientId}&logout_uri={Uri.EscapeDataString(redirectUri)}";
@@ -31,7 +31,7 @@ namespace BlogMultisuarioApp.Controllers
         public IActionResult Register()
         {
             var clientId = "6nej8qanjv13q0kobf3glnr9d2"; // tu Client ID
-            var redirectUri = "https://blogmultiusuarioapp-env.eba-h8cibmhk.us-east-1.elasticbeanstalk.com/Account/Login"; // debe coincidir con lo configurado en Cognito
+            var redirectUri = "https://tajama03-env.eba-3se5iiix.us-east-1.elasticbeanstalk.com/Account/Login"; // debe coincidir con lo configurado en Cognito
             var domain = "us-east-1vtFErItEI.auth.us-east-1.amazoncognito.com"; // tu dominio de Cognito
 
             var signupUrl = $"https://{domain}/signup?" +
